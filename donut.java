f77ef3dda315f1066284eaa916b91367b42620cb
JasonChan-5 <71650687+JasonChan-5@users.noreply.github.com>
2022-07-26 23:32:10 -0400

    adding multithread attempt files

--- donut.java
+++ donut.java
@@ -6,9 +6,12 @@ import javax.swing.JComponent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.Math;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class donut{
 
+    private static HashSet<int[]> coords = new HashSet<>();
     public static void main(String args[]){
         JFrame f = new JFrame("donut.java");
         f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -25,11 +28,31 @@ public class donut{
         float R2 = 125; //x offset    
         double A = 0;
         double B = 0;
+        mThread thread1 = new mThread();
+        mThread thread2 = new mThread();
+        mThread thread3 = new mThread();
+        mThread thread4 = new mThread();
         while (true){
-            render(A, B, d, R1, R2);
-            f.setVisible(true);
+            thread1.r(A, B, R1, R2);
             A += 1;
             B += 1;
+            System.out.println("thread 1");
+            thread2.d(d);
+            System.out.println("thread 2");
+            thread3.r(A, B, R1, R2);
+            A += 1;
+            B += 1;
+            System.out.println("thread 3");
+            thread4.d(d);
+            System.out.println("thread 4");
+            // thread3.run(A, B, d, R1, R2);
+            // A += 1;
+            // B += 1;
+            // System.out.println("thread 3");
+            // thread4.run(A, B, d, R1, R2);
+            // A += 1;
+            // B += 1;
+            // System.out.println("thread 4");
         }
     }
 
@@ -43,27 +66,36 @@ public class donut{
 
     public static class drawDonut extends JComponent{}
 
-    public static void render(double A, double B, drawDonut d, float R1, float R2){
+    public static void render(double A, double B, float R1, float R2){
+        //rotation math starts here
+        for (double i = 0; i < 361; i += 5){ //2d
+            for (double p = 0; p < 361; p += 5){ //3d
+                int x = Math.round((R2 + R1 * cos(i)) * (cos(B) * cos(p) + sin(A) * sin(B) * sin(p)) - R1 * cos(A) * sin(B) * sin(i));
+                int y = Math.round((R2 + R1 * cos(i)) * (cos(p) * sin(B) - cos(B) * sin(A) * sin(p)) + R1 * cos(A) * cos(B) * sin(i));
+                int[] a = {x, y};
+                coords.add(a);
+            }
+        }
+        try{
+            Thread.sleep(95);
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    public static void draw(drawDonut d){
         Graphics2D g = (Graphics2D) d.getGraphics();
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 500, 500);
         g.setColor(Color.WHITE);
         g.translate(250, 250);
-        //rotation math starts here
-        for (double i = 0; i < 361; i += 2){ //2d
-            for (double p = 0; p < 361; p += 2){ //3d
-                int x = Math.round((R2 + R1 * cos(i)) * (cos(B) * cos(p) + sin(A) * sin(B) * sin(p)) - R1 * cos(A) * sin(B) * sin(i));
-                int y = Math.round((R2 + R1 * cos(i)) * (cos(p) * sin(B) - cos(B) * sin(A) * sin(p)) + R1 * cos(A) * cos(B) * sin(i));
-                g.drawString(".", x, y);
-            }
+        Iterator<int[]> i = coords.iterator();
+        while (i.hasNext()){
+            int[] a = i.next();
+            g.drawString(".", a[0], a[1]);
         }
-        d.update(g);
-        try{
-            Thread.sleep(90);
-        }
-        catch(Exception e){
-
-        }
+        coords.clear();
     }
 
     public static float cos(double x){
@@ -75,7 +107,23 @@ public class donut{
     }
 
 }
-//p = phi
-// (R2 + R1 * cos(i)) * (cos(B) * cos(p) + sin(A) * sin(B) * sin(p)) - R1 * cos(A) * sin(B) * sin(i) x
-// (R2 + R1 * cos(i)) * (cos(p) * sin(B) - cos(B) * sin(A) * sin(p)) + R1 * cos(A) * cos(B) * sin(i) y
-// cos(A) * (R2 + R1 * cos(i)) * sin(phi) + R1 * sin(A) * sin(i) 
+
+class mThread extends Thread{
+    
+    public void run(double A, double B, donut.drawDonut d, float R1, float R2){
+        try{
+            System.out.println("Running");
+        }
+        catch (Exception e){
+
+        }
+    }
+
+    public void r(double A, double B, float R1, float R2){
+        donut.render(A, B, R1, R2);
+    }
+
+    public void d(donut.drawDonut d){
+        donut.draw(d);
+    }
+}
